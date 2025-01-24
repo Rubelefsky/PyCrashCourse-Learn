c92bea870163087bd230e95fbcf70b0c45ba6f30
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-01-24 13:57:01 +0000

    Adding files

---
+++ python_work/name.py
@@ -0,0 +1,4 @@
+name = "ada lovelace" # variable srting ada lovelace
+print(name.title()) # makes first and last name capital
+print(name.upper()) # makes all letters capital
+print(name.lower()) # makes all letters lowercase
---
+++ python_work/whitespace.py
@@ -0,0 +1,4 @@
+print("Python")
+print("\tPython") # \t = tab
+print("Languages:\nPython\nC\nJavascript") # \n = new line
+print("Languages:\n\tPython\n\tC\n\tJavascript") # \n\t = new line and tab
