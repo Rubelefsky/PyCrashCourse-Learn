599979939d3addb8d49891f74b9f1508fda1e411
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-01-24 19:54:56 +0000

    Added chapter 2 and ch2 practice

---
+++ python_work/Chapter2/stripping.py
@@ -0,0 +1,6 @@
+# Removing Prefixes
+rubelefsky_url = 'https://www.rubelefsky.com' # Var named rubelefsky_url
+simple_url = rubelefsky_url.removeprefix('https://') # Var named simple_url with removeprefix method
+print(simple_url) # Print simple_url
+
+
