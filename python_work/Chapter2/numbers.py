20374f4220f8f3a15b25c3ff7670249b671e487a
Brandon Rubell <brandonrubell@BRM4Pro.local>
2025-01-26 07:41:26 -0500

    Adding numbers.py

---
+++ python_work/Chapter2/numbers.py
@@ -0,0 +1,11 @@
+addition = 2 + 3 # Addition
+print(addition)
+
+multiply = 2 * 4 # Multiplication
+print(multiply)
+
+division = 12 / 3 # Division
+print(division)
+
+subtraction = 2 - 1 # Subtraction
+print(subtraction)  
