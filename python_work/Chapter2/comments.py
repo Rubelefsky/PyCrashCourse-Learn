58f240a8da528f41177c4db37054b9ad45e0e7a9
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-05 13:28:56 +0000

    adding file comments

---
+++ python_work/Chapter2/comments.py
@@ -0,0 +1,7 @@
+# Say hello
+print("Hello everyone!")
+
+# Main reason to write comments it to explain code to others.
+# Comments are ignored by the Python interpreter.
+# Comments can be on their own line or at the end of a line of code.
+# Comments can be on the same line as code.
