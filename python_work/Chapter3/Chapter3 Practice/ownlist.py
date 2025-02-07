afe9aaf14ac330a5666b8dde7f995536d9fdce35
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-07 12:39:39 +0000

    added ownlist.py

--- python_work/Chapter3/Chapter3 Practice/ownlist.py
+++ python_work/Chapter3/Chapter3 Practice/ownlist.py
@@ -1,1 +1,10 @@
 # 3.3 Create your own list
+past_vehicles = ["2010 Honda Accord", "Jetta GLI", "Kia Sephia", "2002 Honda Accord"]
+print(past_vehicles)
+print(past_vehicles[0])
+
+first_car = f"My first car was a {past_vehicles[0].title()}. It was an automatic."
+print(first_car)
+
+favorite_car = f"My favorite car was my {past_vehicles[1]}. It was a stick shift."
+print(favorite_car)
