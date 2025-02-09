151b6d15f981b85f66c632ed06c8dadd60cf326e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-09 11:58:36 +0000

    remove_value.py add

--- python_work/Chapter3/remove_value.py
+++ python_work/Chapter3/remove_value.py
@@ -1,1 +1,16 @@
-# Remove bny a value
+# Remove by a value
+motorcycles = ['honda', 'ducati', 'yamaha', 'bmw']
+print(motorcycles)
+
+motorcycles.remove('bmw') # Remove the BMW Value
+print(motorcycles)
+
+too_expensive = 'ducati' # Creates the variable too_expensive with the ducati value
+motorcycles.remove(too_expensive) # When removes the too_expensive value which is ducati
+
+print(motorcycles)
+
+print(too_expensive)
+
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
