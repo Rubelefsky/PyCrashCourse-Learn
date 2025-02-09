910a9747c903fe524b6863230dbbcfbf4d0d7ab3
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-09 12:06:37 +0000

    Adding guest_list.py

---
+++ python_work/Chapter3/Chapter3 Practice/guest_list.py
@@ -0,0 +1,13 @@
+# 3.4 Create a guest list
+
+guest_list = ['George Washington', 'Richard Winters', 'Leonardo Da Vinci'] # Creates the guest list 
+
+president = "George Washington" # variable President is assigned to George Washington
+guest_list.remove(president) # Removes George Washington from the list 
+
+print(president)
+
+print(guest_list)
+
+Invite_presidents = f"I would love to invite {president.title()}." # creates variable with the message I would love to invite George Washington.
+print(Invite_presidents)
