97f8eb51bc08f35aca7f05ebe3e14d091f5cc3fd
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-09 15:00:32 +0000

    started change_guest.py

--- python_work/Chapter3/Chapter3 Practice/change_guest.py
+++ python_work/Chapter3/Chapter3 Practice/change_guest.py
@@ -1,1 +1,12 @@
 # 3.5 Changing Guest List - Page 42
+
+guest_list = ['scott', 'brandon', 'steven', 'russ']
+print(guest_list)
+
+guest_list.remove('scott') # Remove the value scott
+print(guest_list)
+
+guest_list.append('matt') # add Matt to the end of the list
+print(guest_list)
+
+
