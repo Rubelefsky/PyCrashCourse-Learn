623b028ae58507f8e6e4e963f9b26d34dea62303
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-09 19:08:35 +0000

    More adding to change_guest.py

--- python_work/Chapter3/Chapter3 Practice/change_guest.py
+++ python_work/Chapter3/Chapter3 Practice/change_guest.py
@@ -6,5 +6,12 @@ print(guest_list)
 
 print(f"Here is the guest list. \n{guest_list[0].title()}\n{guest_list[1].title()}\n{guest_list[2].title()}\n{guest_list[3].title()}")
 
+leaving_guest = 'scott'
+guest_list.remove(leaving_guest)
+
+print(leaving_guest)
+
+leaving_guest_message = f"{leaving_guest.title()} will not be joining us tonight."
+print(leaving_guest_message)
 
 
