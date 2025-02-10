6ed83ab7450586fee41dd498d180ff2cbe1fbbe1
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-10 12:13:27 +0000

    Making changes shrinking_guest.py

--- python_work/Chapter3/Chapter3 Practice/shrinking_guest.py
+++ python_work/Chapter3/Chapter3 Practice/shrinking_guest.py
@@ -1,1 +1,12 @@
 # 3.7 Shrinking Guest List - Page 42
+guest_list = ['Joe', 'Steve', 'Mike', 'Dan']
+message = 'Only 2 people can make it tonight.'
+
+print(message)
+
+guest_1 = guest_list.pop(0) # Assigns guest 1 to Joe
+guest_2 = guest_list.pop(1) # Assigns guest 2 to Steve
+
+print(guest_1)
+print(guest_2)
+
