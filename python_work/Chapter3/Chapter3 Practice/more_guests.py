38dfa17f5d887e855b4007e85b90e080dab78ad3
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-10 00:02:30 +0000

    More_guests.py

--- python_work/Chapter3/Chapter3 Practice/more_guests.py
+++ python_work/Chapter3/Chapter3 Practice/more_guests.py
@@ -1,1 +1,16 @@
 # 3.6 More Guest = Page 42
+
+guest_list = ['Joe', 'Steve', 'Mike', 'Dan']
+print(guest_list)
+
+guest_list.insert(0, 'Jack')
+print(guest_list)
+
+guest_list.insert(3, 'John')
+print(guest_list)
+
+guest_list.append('Joel')
+print(guest_list)
+
+invitation_jack = f"Please save the date, {guest_list[0].title()}."
+print(invitation_jack)
