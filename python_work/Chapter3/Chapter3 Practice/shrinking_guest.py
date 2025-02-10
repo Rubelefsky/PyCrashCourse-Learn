e48f4fdb9b23b5d3da110d6632418a9901227720
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-10 12:19:01 +0000

    Finished shrinking guest list

--- python_work/Chapter3/Chapter3 Practice/shrinking_guest.py
+++ python_work/Chapter3/Chapter3 Practice/shrinking_guest.py
@@ -10,3 +10,12 @@ guest_2 = guest_list.pop(1) # Assigns guest 2 to Steve
 print(guest_1)
 print(guest_2)
 
+new_message = f"{guest_1.title()} and {guest_2.title()} will be joining us for dinner tonight."
+print(new_message)
+
+print(guest_list)
+
+del guest_list[0]
+del guest_list[0]
+
+print(guest_list)
