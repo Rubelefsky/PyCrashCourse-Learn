1b6eb9d359edb7c491d273a12a94ad2071f4e6e9
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-12 11:54:40 +0000

    Added reverseprint, tempsort.py

---
+++ python_work/Chapter3/printreverse.py
@@ -0,0 +1,9 @@
+# This will reverse the original order of the list
+cars = ['bmw', 'toyota', 'porsche', 'honda']
+print(cars)
+
+cars.reverse() # applies permanently
+print(cars)
+
+cars.reverse() # Revert to the original order by applying reverse again
+print(cars)
---
+++ python_work/Chapter3/tempsort.py
@@ -0,0 +1,11 @@
+# This will temporarily sort the list
+cars = ['bmw', 'toyota', 'porsche', 'honda']
+
+print("Here is the original list.")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
