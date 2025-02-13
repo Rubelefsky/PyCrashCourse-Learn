87d8133f4513f9007148c5bcd9382015afb345af
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-13 12:28:54 +0000

    ADding seeingtheworld.py

--- python_work/Chapter3/Chapter3 Practice/seeingtheworld.py
+++ python_work/Chapter3/Chapter3 Practice/seeingtheworld.py
@@ -1,1 +1,12 @@
 # 3.8 Store a list of 5 locations in the world to visit.
+locations = ['Italy', 'Spain', 'Japan', 'Morocco']
+print(locations) # Print List
+
+print("\nThis is the sorted list of locations:")
+print(sorted(locations))
+
+print("\nThis it the original printed again:")
+print(locations)
+
+print("\nThis is the reverse list")
+print
