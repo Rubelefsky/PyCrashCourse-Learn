c27aa352d93e615f603eaa25c0dff6ce96b4cf6a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-16 17:52:43 +0000

    Changed folder names and added chapter 4

---
+++ python_work/Chapter3 - Lists/indexerror.py
@@ -0,0 +1,7 @@
+# Avoing index errors.
+motorcycles = ['honda', 'yamaha', 'suzuki'] # 3 Items in list
+print(motorcycles[3]) # Asking for 4th item in list will produce an index error. List index out of range
+
+# Use [-1] to get the list item on a list
+
+
