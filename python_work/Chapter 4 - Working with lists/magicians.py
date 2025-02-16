9e6f4f42de4d3d1f32f061bba9e546d4f8018a8e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-16 17:56:47 +0000

    Adding magicians.py

---
+++ python_work/Chapter 4 - Working with lists/magicians.py
@@ -0,0 +1,6 @@
+# Looping through an entire list
+magicians = ['Carol', 'John', 'Jose'] # create list
+for magician in magicians: # loop through all magicians in magicians. For every magician in the list of magicians, print the magicians name.
+    print(magician) # Prints the list
+
+    
