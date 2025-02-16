fcf3e2e0c893ea9eee4892003f1a226dd309782a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-16 15:29:03 +0000

    dinnerguests.py

---
+++ python_work/Chapter3/Chapter3 Practice/dinnerguest.py
@@ -0,0 +1,7 @@
+# 3.9 use len to get the number of dinner guests.
+
+dinner_guests = ['Brandon', 'Scott', 'Steve', 'John']
+print(dinner_guests)
+
+number_guests = f"The number of guests is {len(dinner_guests)}."
+print(number_guests)
