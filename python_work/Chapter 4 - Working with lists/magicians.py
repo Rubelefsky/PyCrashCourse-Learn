e724430dde26e5db6a8ff784c48630ac5ea2e578
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-17 00:17:23 +0000

    working

--- python_work/Chapter 4 - Working with lists/magicians.py
+++ python_work/Chapter 4 - Working with lists/magicians.py
@@ -6,6 +6,9 @@ for magician in magicians: # loop through all magicians in magicians. For every 
 
 cats = ['Jerry', 'Paulie', 'Sandy']
 for cat in cats:
-    print(cat)
+    print(f"{cat.title()} is very cuddly!") # Every indented line is considered in the loop.
+    print(f"I wonder if {cat.title()} will hunt for mice.\n") # The newline at the end will make everything more neat. 
+
+print("Even though I am a dog person, those cats were cute.") # Not indented so not in the loop
 
     
