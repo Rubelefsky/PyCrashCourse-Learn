c117ff98c7e659787eae49cfa1fe10147f15b5e7
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-17 11:21:32 +0000

    added square_numbers.py

---
+++ python_work/Chapter 4 - Working with lists/square_numbers.py
@@ -0,0 +1,6 @@
+squares = [] # Empty List
+for value in range(1, 11): # Loop through 1-10
+    square = value ** 2 # Each value gets raised to the second power
+    squares.append(square) # Each new value gets added to the list
+
+print(squares) # prints the list which has the new values added
