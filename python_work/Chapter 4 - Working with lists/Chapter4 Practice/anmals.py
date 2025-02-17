63fbfe1a37aa384de2b2ed5ace7916b5c4e8eb0d
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-17 00:27:40 +0000

    Pizza and animals. My favorite.

---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/anmals.py
@@ -0,0 +1,9 @@
+#4.2 Create a list of animals with the same characteristics.
+# Create a list
+# Print in a for loop
+
+animals = ['dog', 'cat', 'moose']
+for animal in animals:
+    print(f"I have a {animal.title()} as a pet!")
+
+print("\nThese are all reasonable pets.")
---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/pizza.py
@@ -0,0 +1,6 @@
+# 4.1 Create a list of pizzas and make a for loop
+pizzas = ['Chicken Parm', 'Pepperoni', 'Cheese']
+for pizza in pizzas:
+    print(f'{pizza.title()} is one of my favorites.')
+
+print('\nI really love pizza!')
