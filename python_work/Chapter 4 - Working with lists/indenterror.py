bebdce3a498956348be805524ce24411a78b401a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-17 00:22:39 +0000

    removing indenterror.py

--- python_work/Chapter 4 - Working with lists/indenterror.py
+++
@@ -1,3 +0,0 @@
-magicians = ['Carol', 'John', 'Jose'] # create list
-for magician in magicians: # loop through all magicians in magicians. For every magician in the list of magicians, print the magicians name.
-print(magician) # This will not print because it is not indented in the loop and will cause an indentation error
