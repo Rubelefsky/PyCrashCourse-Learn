e4e49618eeb60093350dccdeb6cde4a6268e07e9
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-18 23:47:46 +0000

    Added comments

--- python_work/Chapter 4 - Working with lists/square_concise.py
+++ python_work/Chapter 4 - Working with lists/square_concise.py
@@ -1,5 +1,5 @@
-squares = []
-for value in range(1,11):
-    squares.append(value**2)
+squares = [] # Empty List
+for value in range(1,11): # Loop through 1-10
+    squares.append(value**2) # Each value gets raised to the second power
 
-print(squares)
+print(squares) # prints the list which has the new values added
--- python_work/Chapter 4 - Working with lists/square_numbers.py
+++ python_work/Chapter 4 - Working with lists/square_numbers.py
@@ -1,5 +1,5 @@
 squares = [] # Empty List
-for value in range(1, 11): # Loop through 1-10
+for value in range(1,11): # Loop through 1-10
     square = value ** 2 # Each value gets raised to the second power
     squares.append(square) # Each new value gets added to the list
 
