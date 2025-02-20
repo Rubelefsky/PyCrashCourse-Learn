b9ad7323cf7c27333b706f71d83fb44239fad6be
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-20 11:59:44 +0000

    ADding digits.py

---
+++ python_work/Chapter 4 - Working with lists/digits.py
@@ -0,0 +1,4 @@
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] # Makes a list 0-9
+min(digits) # In Console prints Minimum number which is 0
+max(digits) # Prints largest number which is 9
+sum(digits) # Adds all of the digits which is 45
