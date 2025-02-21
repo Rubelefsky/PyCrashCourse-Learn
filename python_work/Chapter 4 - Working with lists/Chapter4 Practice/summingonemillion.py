c30cdea490218fa0719c9976aa78c710880b6921
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-21 12:32:12 +0000

    Summingonemillion.py

--- python_work/Chapter 4 - Working with lists/Chapter4 Practice/summingonemillion.py
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/summingonemillion.py
@@ -1,1 +1,13 @@
 #4.5 Use min/max on 1/1000000
+numbers = list(range(1, 1000001)) # Create a variable that is a list with a range of 1 - 1000001. 
+print(min(numbers)) #Print the min number of the list
+print(max(numbers)) # PRint the max number of the list
+
+max_number = max(numbers) # create a variable for the max number
+min_number = min(numbers) # Create a variable for the min number
+
+print(min_number + max_number) # add both variables together to get 1000001
+
+
+
+
