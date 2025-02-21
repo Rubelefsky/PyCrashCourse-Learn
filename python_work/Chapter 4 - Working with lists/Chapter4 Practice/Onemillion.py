92d81d47e65c312160e2d4885888d4652353c4b7
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-21 12:27:57 +0000

    Added files

---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/Onemillion.py
@@ -0,0 +1,3 @@
+#4.4 Make a list of the numbers from one to one million.
+for numbers in range(1, 1000000):
+    print(numbers)
--- python_work/Chapter 4 - Working with lists/Chapter4 Practice/countingtotwenty.py
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/countingtotwenty.py
@@ -1,1 +1,4 @@
 #4.3 Counting to Twenty - Use a for loop to print numbers 1-20
+for number in range(1,21):
+    print(number)
+    
