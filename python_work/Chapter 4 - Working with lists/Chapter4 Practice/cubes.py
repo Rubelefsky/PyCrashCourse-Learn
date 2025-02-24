3403da18480b7baa6bd2e4be2f4ab7958ec1a99a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-02-24 12:04:56 +0000

    added cubes.py and completed

---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/cubes.py
@@ -0,0 +1,6 @@
+cubes = [] # Create Empty list
+for value in range(1, 11): # Loop through 1-10
+    cube = value ** 3 # Each value raised to the 3rd power (cubed)
+    cubes.append(cube) # append each value to the list cubes
+
+print(cubes) # Prints list where values have been added/appended
