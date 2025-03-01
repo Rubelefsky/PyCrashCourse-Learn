f5d247a91662601def00903d273f76b07b8e4407
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-01 23:22:56 +0000

    Add more_loops.py

---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/more_loops.py
@@ -0,0 +1,13 @@
+# 4.12 Choose a versions of foods.py and make a for loop / write 2 for loops
+
+my_foods = ['pizza', 'falafel', 'hamburgers', 'bagel']
+print(my_foods)
+
+for food in my_foods:
+    print(food.title())
+
+
+print("My favorite foods are:")
+for food in my_foods:
+    print(food.upper())
+    
