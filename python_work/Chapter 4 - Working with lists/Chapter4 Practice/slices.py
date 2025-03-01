f5e089455c45e25cf75476eb6dcbb1aac4a1463d
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-01 23:10:23 +0000

    Add

---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/slices.py
@@ -0,0 +1,13 @@
+# 4.1 Slices
+animals = ['jackrabbit', 'horse', 'tiger', 'lion']
+print(animals)
+
+print("The first three items in the list are:")
+print(animals[:3]) # Prints first three animals
+
+print("Three items in the middle of the list are:")
+print(animals[1:3]) # Prints the middle animals
+
+print("The last three items in the list are:")
+print(animals[-3:]) # prints the last three
+
