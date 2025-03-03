7aa0f52df5f8b7fd9c7a247613836d87b27f047a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-03 23:55:56 +0000

    cars.py

---
+++ python_work/Chapter 5 - IF Statements/cars.py
@@ -0,0 +1,7 @@
+cars = ['audi', 'bmw', 'toyota', 'honda']
+
+for car in cars: # Starts Loop
+    if car == 'bmw': # If car is equal to (==) BMW
+        print(car.upper()) # Print all uppercase
+    else:
+        print(car.title()) # Else print title of car
