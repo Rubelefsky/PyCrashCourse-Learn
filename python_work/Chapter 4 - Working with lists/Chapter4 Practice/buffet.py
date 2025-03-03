32d56c8c5a0ae59155672c89d7b5945e403bf039
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-03 13:52:01 +0000

    Favorite_foods.py

---
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/buffet.py
@@ -0,0 +1,8 @@
+# 4.13 Store five basic foods in a tuple
+
+favorite_foods = ('hamburger', 'pasta', 'bagels', 'strawberry')
+print(favorite_foods)
+
+print("My favorites food are:")
+for food in favorite_foods:
+    print(food.title())
