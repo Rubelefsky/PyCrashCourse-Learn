46bc9ed3331ec09c20d00eb020fca0dc6f643592
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-03 13:52:35 +0000

    Favorite_foods.py

--- python_work/Chapter 4 - Working with lists/Chapter4 Practice/buffet.py
+++ python_work/Chapter 4 - Working with lists/Chapter4 Practice/buffet.py
@@ -1,8 +1,8 @@
 # 4.13 Store five basic foods in a tuple
 
-favorite_foods = ('hamburger', 'pasta', 'bagels', 'strawberry')
-print(favorite_foods)
+favorite_foods = ('hamburger', 'pasta', 'bagels', 'strawberry') # Create tuple
+print(favorite_foods) # print list(tuple)
 
 print("My favorites food are:")
-for food in favorite_foods:
-    print(food.title())
+for food in favorite_foods: # For loop of tuple
+    print(food.title()) # Print each food with capital
