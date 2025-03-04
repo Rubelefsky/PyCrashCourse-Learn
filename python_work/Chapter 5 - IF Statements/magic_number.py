10b0020465e09c57f6dfe058f74a3a76194aae12
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-04 12:28:42 +0000

    magic_number.py

---
+++ python_work/Chapter 5 - IF Statements/magic_number.py
@@ -0,0 +1,5 @@
+answer = 17 # assign variable to 17
+if answer != 42: # IF the answer is not equal to 42
+    print("That is not the correct answer. Please try again!") # Print if not !=
+else:
+    print("That is the correct answer!") # Print if ==
