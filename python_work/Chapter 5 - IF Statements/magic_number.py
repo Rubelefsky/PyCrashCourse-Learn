b337a8fd263100d3173b73a1186a98e624892851
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-04 12:38:22 +0000

    add magic_numbers.py

--- python_work/Chapter 5 - IF Statements/magic_number.py
+++ python_work/Chapter 5 - IF Statements/magic_number.py
@@ -3,3 +3,18 @@ if answer != 42: # IF the answer is not equal to 42
     print("That is not the correct answer. Please try again!") # Print if not !=
 else:
     print("That is the correct answer!") # Print if ==
+
+# Can include various comparisons
+# age = 19
+# age < 20 == True
+# age > 20 == False
+
+# Use AND to compare two conditions simultaneously
+# age_0 = 22
+# age_1 = 18
+# age_0 >= 21 and age_1 >= 21 == FALSE 
+# Age_0 is greater than 21 AND age_1 is not greater than 21 there it is FALSE
+
+# age_1 = 22
+# age_0 >= 21 and age_1 >= 21 
+# Equals to true as age_0 is 22 which is greater than 21 and age_1 is 22 which is also greater than 21
