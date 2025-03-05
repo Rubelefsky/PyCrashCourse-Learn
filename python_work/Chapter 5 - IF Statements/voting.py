f7a583018bc5c1699fc104e2d16a670a36902304
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-05 02:08:45 +0000

    voting.py

---
+++ python_work/Chapter 5 - IF Statements/voting.py
@@ -0,0 +1,8 @@
+age = 7 # variable assigns age to 19
+if age >= 18: # checks if age is greater than or equal to 19
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")# if the value was less than 18 there would be no output
+else:
+    print("Sorry you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
