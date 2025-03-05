c5a14240e4dd1fd206ba1f99507e78f12ea6298c
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-05 12:19:21 +0000

    add single_print.py

---
+++ python_work/Chapter 5 - IF Statements/single_print.py
@@ -0,0 +1,11 @@
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"Your admission price is ${price}.")
+
+# Shortened version of python_work/Chapter 5 - IF Statements/amusement_park.py
