de3ea8af559e435578a5a97885e37e71c4ec64c9
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 23:09:17 +0000

    favorite_fruits.py

--- python_work/Chapter 5 - IF Statements/Chapter5Practice/favorite_fruit.py
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/favorite_fruit.py
@@ -1,1 +1,9 @@
 # 5.7 Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+favorite_fruits = ['strawberry', 'blueberry', 'banana']
+
+if 'strawberry' in favorite_fruits:
+    print('I love strawberries.')
+if 'blueberry' in favorite_fruits:
+    print("I love blueberries.")
+if 'banana' in favorite_fruits:
+    print("I love bananas.")
