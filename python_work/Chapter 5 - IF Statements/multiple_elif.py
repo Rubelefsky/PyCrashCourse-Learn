ea7f36ed698ffaef4812fe05cb1c1d0ae258962c
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 12:11:46 +0000

    Add multiple_elif.py

---
+++ python_work/Chapter 5 - IF Statements/multiple_elif.py
@@ -0,0 +1,12 @@
+age = 19
+
+if age < 4: # IF age less than 4
+    price = 0 # Price is 0
+elif age < 18: # IF age less than 18 
+    price = 25 # Price is 25
+elif age < 65: # IF age less than 65
+    price = 40 # Price is 40
+else: # ELSE 
+    price = 20 # Price is 20
+
+print(f"Your admission price cost is {price}.")
