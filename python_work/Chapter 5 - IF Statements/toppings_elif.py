8dc2bef065dd50cdd248563bdcf4398109507941
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 12:24:36 +0000

    Adding toppings_elif.py

---
+++ python_work/Chapter 5 - IF Statements/toppings_elif.py
@@ -0,0 +1,13 @@
+requested_toppings = ['mushrooms', 'extra cheese'] # list with toppings
+
+if 'mushrooms' in requested_toppings: # IF mushrooms is in requested_toppings list
+    print("Adding Mushrooms.")
+if 'pepperoni' in requested_toppings: # IF pepperoni is in requested_toppings list
+    print("Adding Pepperoni.")
+if 'extra cheese' in requested_toppings: # IF extra cheese is in requested_toppings list
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+# IF statements, tests will run if previous test passed or not
+# Elif statement, tests will stop after first test passes
