84d8cb43e3e33f230d672b19995bb6e01d910991
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 19:34:32 +0000

    stages_of_life.py?

--- python_work/Chapter 5 - IF Statements/Chapter5Practice/stages_of_life.py
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/stages_of_life.py
@@ -1,1 +1,18 @@
 # 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life.
+age = 67
+
+if age < 2:
+    stage = 'baby'
+elif age < 4:
+    stage = 'toddler'
+elif age < 13:
+    stage = 'kid'
+elif age < 20:
+    stage = 'teenager'
+elif age < 65:
+    stage = 'adult'
+else:
+    stage = 'elder'
+
+
+print(f"You are a {stage}.")
