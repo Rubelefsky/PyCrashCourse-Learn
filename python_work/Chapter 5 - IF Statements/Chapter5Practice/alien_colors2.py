53fef011e5d7e6a673299a276684799605f1f6c0
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 13:43:50 +0000

    alien_color2 and 3

--- python_work/Chapter 5 - IF Statements/Chapter5Practice/alien_colors2.py
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/alien_colors2.py
@@ -1,1 +1,6 @@
 # 5.4 Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+alien_color = 'blue' # Assign green color
+if alien_color == 'green':
+    print(f"Player earned 5 points.") # IF color == green, earn 5 points
+else:
+    print(f"Player earned 10 points.") # IF color anything else, earn 10 points
