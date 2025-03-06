952c692e5f249ead88c902a2356b0fd584d0a28c
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 12:30:53 +0000

    Add alien_colors.py

---
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/alien_colors.py
@@ -0,0 +1,7 @@
+# 5.3 Create a variable alien_color and assign it a value of green, red or yellow
+
+alien_color = 'green' # Assign green color
+if alien_color == 'green': # IF color is green print The COlor is green!
+    print(f"The color is {alien_color}!")
+if alien_color == 'red': # IF color == red, print the color is red!
+    print(f"The color is red!")
