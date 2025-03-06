3f6ceb8380633a485c8bbb477e1a3f0d617ab816
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 13:50:21 +0000

    alien_color3short.py

---
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/alien_colors3.py
@@ -0,0 +1,8 @@
+# 5.5 Turn your if-else chain from Exercise 5-4 into an if-elif- else chain.
+alien_color = 'yellow' # Assign green color
+if alien_color == 'green':
+    print(f"Player earned 5 points.") # IF color == green, earn 5 points
+elif alien_color == 'yellow':
+    print(f"Player earned 10 points.") # IF color anything else, earn 10 points
+elif alien_color == 'red':
+    print("Player earned 15 points.")
---
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/alien_colors3short.py
@@ -0,0 +1,12 @@
+#5.5.2
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'blue':
+    points = 10
+else:
+    points = 15
+
+print(f"You have {points} points.")
