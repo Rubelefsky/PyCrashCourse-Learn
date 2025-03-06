1b8f3e70b3ef892410b5338257f8be6873a24cba
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-06 12:19:17 +0000

    add comments

--- python_work/Chapter 5 - IF Statements/multiple_elif.py
+++ python_work/Chapter 5 - IF Statements/multiple_elif.py
@@ -1,4 +1,4 @@
-age = 19
+age = 66
 
 if age < 4: # IF age less than 4
     price = 0 # Price is 0
@@ -6,7 +6,7 @@ elif age < 18: # IF age less than 18
     price = 25 # Price is 25
 elif age < 65: # IF age less than 65
     price = 40 # Price is 40
-else: # ELSE 
+else: # ELSE -Statement is the catchall.Matches any condition that was not matched by a specific IF or ELIF test
     price = 20 # Price is 20
 
 print(f"Your admission price cost is {price}.")
