95a42dea13cfc7e848547043d507cc58c4e2e4b2
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-07 12:21:14 +0000

    hello_admin edit

--- python_work/Chapter 5 - IF Statements/Chapter5Practice/hello_admin.py
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/hello_admin.py
@@ -4,6 +4,6 @@ for username in available_usernames: # loop through list of usernames
     if username == 'admin': # if username is equal to admin, print below
         print(f"Welcome, {username.title()}, Would you like a status report?")
     elif username in available_usernames:# if username is in list and not admin, print below
-        print(f"Welcome, {username.title()}.")
+        print(f"Welcome, {username.title()}. Thank you for logging in.")
     else: # if username is not in list, print below
         print(f"{username} is an incorrect username.")
