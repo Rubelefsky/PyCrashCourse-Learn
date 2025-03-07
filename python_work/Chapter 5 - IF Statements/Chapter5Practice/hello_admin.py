1bbb4bd996b9c79c7535d21d5b7b7f046834c976
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-07 12:28:56 +0000

    added 5.9 no users to hello_admin.py

--- python_work/Chapter 5 - IF Statements/Chapter5Practice/hello_admin.py
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/hello_admin.py
@@ -1,9 +1,15 @@
 available_usernames = ['admin', 'brandon', 'jack', 'smith', 'steve'] # Create a list
 
-for username in available_usernames: # loop through list of usernames
-    if username == 'admin': # if username is equal to admin, print below
-        print(f"Welcome, {username.title()}, Would you like a status report?")
-    elif username in available_usernames:# if username is in list and not admin, print below
-        print(f"Welcome, {username.title()}. Thank you for logging in.")
-    else: # if username is not in list, print below
-        print(f"{username} is an incorrect username.")
+if available_usernames:
+    for username in available_usernames: # loop through list of usernames
+        if username == 'admin': # if username is equal to admin, print below
+            print(f"Welcome, {username.title()}, Would you like a status report?")
+        elif username in available_usernames:# if username is in list and not admin, print below
+            print(f"Welcome, {username.title()}. Thank you for logging in.")
+else: # if username is not in list, print below
+    print(f"We need to find some users.")
+
+# 5.9 No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+# Tested by removing users from list and else statement did print
+
+
