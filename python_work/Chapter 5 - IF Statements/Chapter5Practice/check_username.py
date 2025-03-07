21a94a8de933e7a51a22cfccf1f68de9dca2a059
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-07 12:34:04 +0000

    comments

--- python_work/Chapter 5 - IF Statements/Chapter5Practice/check_username.py
+++ python_work/Chapter 5 - IF Statements/Chapter5Practice/check_username.py
@@ -6,3 +6,6 @@ new_users = ['brandon', 'bob', 'smith', 'jim', 'alex']
 for users in new_users: # For users in New users list
     if users in current_users: # If user is also in current list
         print(f"Hello, {users.title()}.") # print - brandon and smith
+
+# Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+# Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.)
