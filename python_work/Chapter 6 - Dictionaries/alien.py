b93e18ea9e0fb4391ead5d4bfb174a2bc5a6bdd2
Brandon Rubell <brandonrubell@BRM4Pro.local>
2025-03-08 17:24:24 -0500

    alien_0.py

---
+++ python_work/Chapter 6 - Dictionaries/alien.py
@@ -0,0 +1,12 @@
+alien_0 = {'color': 'green', 'points': 5} # Dictionary stores alien color and point value
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+# Dictionary is a collection of key-value pairs
+# Each key is connected to a value
+# A dictionary is wrapped in {}
+# Access a value by giving the dictionary name and placing the key in square brackets
+
+# Every key is connected to its value by a colon
+# Key-value pairs are separated by commas
