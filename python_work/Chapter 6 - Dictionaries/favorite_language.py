eea8ce4fbeed1c18af82f6b8b06b56a01423fbd5
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-09 23:53:51 +0000

    favorite_languages.py

---
+++ python_work/Chapter 6 - Dictionaries/favorite_language.py
@@ -0,0 +1,10 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+print(favorite_languages)
+
+language = favorite_languages['edward'].title() # Creates new variable. Changing the name in the [] will change the output below.
+print(f'Sarahs favorite language is {language}.')
---
+++ python_work/Chapter 6 - Dictionaries/removing_alien.py
@@ -0,0 +1,9 @@
+# Removing Key-Value pairs from dictionary
+alien_0 = {'color': 'green', 'points': 5} # Dictionary stores alien color and point value
+print(alien_0)
+
+del alien_0['points'] # Delete the points key value pair
+print(alien_0)  
+
+# Pair is removed permanently.
+
