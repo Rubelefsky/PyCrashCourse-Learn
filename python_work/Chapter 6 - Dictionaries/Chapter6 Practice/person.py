3adc59e7d66fcadca7ecf1a1dc37c5a734450f90
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-11 11:45:24 +0000

    add person.py

---
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/person.py
@@ -0,0 +1,4 @@
+# 6.1
+
+person = {'first_name': 'brandon', 'last_name': 'scott', 'age': '32', 'city': 'new york'} # Person dictionary
+print(person['first_name'].title()) # print first name with title 
