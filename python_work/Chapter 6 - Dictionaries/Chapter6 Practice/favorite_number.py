60176451cc681ca16ce4322f3258e4b9bdc2a2d8
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-12 11:00:31 +0000

    need to complete

---
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/favorite_number.py
@@ -0,0 +1,8 @@
+# 6.2 Create a dictionary of 5 people and their favorite number
+favorite_numbers = {'carol': 23, 
+                    'brandon': 29, 
+                    'steve': 14, 
+                    'jerry': 49, 
+                    'scott': 46}
+
+print(favorite_numbers['carol'])
---
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/glossary.py
@@ -0,0 +1,14 @@
+# 6.3 Think of five programming words you’ve learned about in the previous chapters. 
+# Use these words as the keys in your glossary, and store their meanings as values.
+
+glossary = {
+    'variable': 'a named storage location that holds a value', 
+    'loop': 'control flow structure that allows a block of code to be executed repeatedly',
+    'if-else': 'conditional true false'
+    }
+
+print(f"A variable is {glossary['variable']}.")
+
+# for word in words:
+#       print(word.title())
+
