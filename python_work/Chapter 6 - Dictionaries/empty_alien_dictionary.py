a7f52f0ed8900aaeff30d2c17fc2d0ffa1d99a36
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-14 21:05:09 +0000

    comment

--- python_work/Chapter 6 - Dictionaries/empty_alien_dictionary.py
+++ python_work/Chapter 6 - Dictionaries/empty_alien_dictionary.py
@@ -5,4 +5,4 @@ alien_0['points'] = 5 # Add to dictionary, Key-Points, Value-5
 
 print(alien_0) # Print dictionary with new key-pair
 
-# Typically, you’ll use empty dictionaries when storing user-supplied data in a dictionary or when writing code that generates a large number of key- value pairs automatically.
+# Typically, you’ll use empty dictionaries when storing user-supplied data in a dictionary or when writing code that generates a large number of key- value pairs automatically
