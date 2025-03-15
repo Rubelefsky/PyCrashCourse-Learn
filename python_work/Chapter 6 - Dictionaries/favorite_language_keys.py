3954795da92fad985f51842dae8012d341fc836b
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-15 15:45:43 +0000

    favorite_language_keys.py

---
+++ python_work/Chapter 6 - Dictionaries/favorite_language_keys.py
@@ -0,0 +1,11 @@
+# Page 101
+# Create a dictionary named favorite_languages
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+for name in favorite_languages.keys():
+    print(name.title())
