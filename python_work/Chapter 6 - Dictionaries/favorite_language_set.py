db25b4785e8e8efa64cad7af2baded3e92bf6c54
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-16 20:15:23 +0000

    add comments

--- python_work/Chapter 6 - Dictionaries/favorite_language_set.py
+++ python_work/Chapter 6 - Dictionaries/favorite_language_set.py
@@ -10,3 +10,5 @@ favorite_languages = {
 print("The following languages have been mentioned:")
 for language in set(favorite_languages.values()): # a **set** is a collection in which each item must be unique.
     print(language.title())
+
+# When wrapping set() around a collection of values that contains duplicate items, Python identifies the unique items in the collection and builds a set from those items.
