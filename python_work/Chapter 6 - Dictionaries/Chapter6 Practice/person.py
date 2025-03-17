20c8889b2059fb623080be0696966c4407f40526
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-17 18:27:58 +0000

    adding edits

--- python_work/Chapter 6 - Dictionaries/Chapter6 Practice/person.py
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/person.py
@@ -1,6 +1,12 @@
 # 6.1
 
-person = {'first_name': 'brandon', 'last_name': 'scott', 'age': '32', 'city': 'new york'} # Person dictionary
+person = {
+    'first_name': 'brandon', 
+    'last_name': 'scott', 
+    'age': '32', 
+    'city': 'new york'
+} # Person dictionary
+
 print(person['first_name'].title()) # print first name with title 
 print(person['last_name'].title()) # Print last name with title
 print(person['age']) # Print age
