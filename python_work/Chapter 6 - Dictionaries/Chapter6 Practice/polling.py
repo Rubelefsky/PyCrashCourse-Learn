3a3885075da6a18815a6bf66f25951ae91ddab86
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-18 17:06:21 +0000

    add

--- python_work/Chapter 6 - Dictionaries/Chapter6 Practice/polling.py
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/polling.py
@@ -3,12 +3,24 @@ favorite_languages = {
   'jen': 'python', 
   'sarah': 'c', 
   'edward': 'rust', 
-  'phil': 'python', 
+  'phil': 'python',
+  'rick': 'java'
 }
 
-polling_list = ['jen', 'brandon', 'phil', 'sarah']
+polling_list = ['jen', 'brandon', 'phil', 'sarah'] # list
 
-for name in polling_list:
-    print(f"Hello {name.title()}, you are in the polling list.")
+# Can use favorite_languages.keys() for easier readability. favorite_languages defaults to keys 
+for name in favorite_languages.keys(): # Pull all keys from dictionary and assign them one at a time to variable "name"
+    print(f"Hello {name.title()}, you are in the favorite languages dictionary.")
+
+    if name in polling_list: # 
+        polling_name = favorite_languages[name].title()
+        print(f"\t{name.title()}, you are in the polling list as well.")
+
+    else:
+        print(f"\t{name.title()}, you are not in the polling list.")
+
+        
+        
 
 
