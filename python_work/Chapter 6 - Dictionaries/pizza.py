58ff2209ec7d214b1f8857a88296ec13896dabfc
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-19 12:16:00 +0000

    add pizza.py

---
+++ python_work/Chapter 6 - Dictionaries/pizza.py
@@ -0,0 +1,15 @@
+# Store information about a pizza being ordered
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+# Summarize the order
+print(f"You ordered a {pizza['crust']} - crust pizza "
+      "with the following toppings:")
+# When breaking up a long line in print
+# choose an appropriate point at which to break the line being printed, and end the line with a quotation mark
+# Indent the next line, add an opening quotation mark, and continue the string. 
+
+for topping in pizza['toppings']: # For loop, use key - toppings
+    print(f"\t{topping}")
