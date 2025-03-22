ca76d57134d81a779c9655ec909cf791d816f2ae
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-22 18:03:48 +0000

    add more to people.py

--- python_work/Chapter 6 - Dictionaries/Chapter6 Practice/people.py
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/people.py
@@ -6,12 +6,27 @@ people = {
         'first_name': 'brandon', 
         'last_name': 'scott', 
         'age': '32', 
-        'city': 'new york',
+        'city': 'manhattan',
     },
+    'adaniels': {
+        'first_name': 'adam', 
+        'last_name': 'daniels', 
+        'age': '28', 
+        'city': 'brooklyn',
+    }
 }
 
-for person, person_info in people.items():
-    print(f"Person: {person.title()}")
-    full_name = f"{person_info['first_name']} {person_info['last_name']}"
-    print(f"{full_name.title()}")
+people_list = [people['brubell'], people['adaniels']]
 
+for person, person_info in people.items(): # assign key/value variables - person and person_info
+    full_name = f"{person_info['first_name']} {person_info['last_name']}"
+    age = f'{person_info['age']}'
+    city = f'{person_info['city']}'
+    
+    print(f"\nPerson: {person.title()}")
+    print(f"Full Name: {full_name.title()}")
+    print(f"Age: {age}")
+    print(f"City: {city.title()}")
+
+
+print(f"\n{people_list[0]}")
