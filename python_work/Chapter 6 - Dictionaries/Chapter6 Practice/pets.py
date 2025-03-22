c6be9cf4e2c4dc5b70bf3d2df49e494e2a0f1e3c
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-22 20:20:11 +0000

    add pets.py

---
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/pets.py
@@ -0,0 +1,20 @@
+# 6.8 Make several dictionaries, representing a different pet.
+pets = {
+    'dog': {
+        'breed': 'poodle',
+        'name': 'corduroy'
+    },
+    'cat': {
+        'breed': 'calico',
+        'name': 'jackson'
+    }
+}
+
+pet_list = [pets['cat'], pets['dog']]
+
+for pet, pet_info in pets.items():
+   print(f'\nPet: {pet.title()}')
+   print(f'Breed: {pet_info['breed'].title()}')
+   print(f'Name: {pet_info['name'].title()}')
+
+# This is def not right - I need to re do this dictionaries chapter.
