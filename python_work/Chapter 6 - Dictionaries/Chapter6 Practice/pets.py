71d4158715e9d2a90be5d231d04ff334bfa19b25
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-22 22:38:50 +0000

    made one line that goes over info

--- python_work/Chapter 6 - Dictionaries/Chapter6 Practice/pets.py
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/pets.py
@@ -13,8 +13,10 @@ pets = {
 pet_list = [pets['cat'], pets['dog']]
 
 for pet, pet_info in pets.items():
-   print(f'\nPet: {pet.title()}')
-   print(f'Breed: {pet_info['breed'].title()}')
-   print(f'Name: {pet_info['name'].title()}')
+   # print(f'\nPet: {pet.title()}')
+   # print(f'Breed: {pet_info['breed'].title()}')
+   # print(f'Name: {pet_info['name'].title()}')
+
+   print(f'\nPet: {pet.title()}\nBreed: {pet_info['breed'].title()}\nName: {pet_info['name'].title()}')
 
 # This is def not right - I need to re do this dictionaries chapter.
