3290f2c02208ebca2e122300321a3f00d295d9a0
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-23 22:09:34 +0000

    parrot.py

---
+++ python_work/Chapter7 - Input/parrot.py
@@ -0,0 +1,4 @@
+# Page 114
+
+message = input('Tell me something, and I will repeat it back to you: ') # input function takes one argument: the prompt that we want to display
+print(message) # the input is assigned to the 'message' variable
