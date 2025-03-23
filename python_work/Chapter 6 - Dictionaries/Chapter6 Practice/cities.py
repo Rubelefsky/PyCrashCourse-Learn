8103e661260d13cd653260351524a65d8bb573a0
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-23 13:15:08 +0000

    started cities.py

--- python_work/Chapter 6 - Dictionaries/Chapter6 Practice/cities.py
+++ python_work/Chapter 6 - Dictionaries/Chapter6 Practice/cities.py
@@ -1,1 +1,24 @@
 # Page 111 6.10
+# Use the names of three cities as keys in your dictionary. 
+# Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city.
+
+cities = {
+    'new york city': {
+        'state': 'new york',
+        'region': 'north east',
+
+    },
+    'chicago': {
+        'state': 'illinois',
+        'region': 'midwest',
+    },
+    'los angeles': {
+        'state': 'california',
+        'region': 'west',
+    }
+
+}
+
+for city, city_info in cities.items():
+    print(city.title())
+    print(city_info)
