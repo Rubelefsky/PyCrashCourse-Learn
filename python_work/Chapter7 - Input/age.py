7bfa01d3e05fdd26935350fbe375f1118dac84e7
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-24 18:50:54 +0000

    age and greeter.py

---
+++ python_work/Chapter7 - Input/age.py
@@ -0,0 +1,8 @@
+# Page 115
+
+age = input("How old are you? ")
+# Prints as a string not a number
+# Will print as '32' in the console/terminal
+age = int(age) # int() function, converts the input string to a numerical value
+age >= 18
+# True
