b51350c73310537c0e14d5157c775f0367bf423e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-03-24 18:58:53 +0000

    add more comments

--- python_work/Chapter7 - Input/rollercoaster.py
+++ python_work/Chapter7 - Input/rollercoaster.py
@@ -2,7 +2,7 @@
 height = input("How tall are you, in inches? ")
 height = int(height) # Convert input to integer
 
-if height >= 48: # If height is greater or equal to 48:
+if height >= 48: # If height is greater or equal to 48. Able to be compared because of int() converting string to int above
     print("\nYou're tall enough to ride!") # Print this if greater or equal
 else:
     print("\nYou'll be able to ride when you are a little older.") # If not greater or equal print this
