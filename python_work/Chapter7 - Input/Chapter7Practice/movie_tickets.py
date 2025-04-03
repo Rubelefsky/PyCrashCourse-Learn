6a0f252628fa3510c7cd1c853643bab2c160fe93
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-03 12:46:02 +0000

    add movie_ticket.py

---
+++ python_work/Chapter7 - Input/Chapter7Practice/movie_tickets.py
@@ -0,0 +1,10 @@
+# 7.5 
+age = int(input("What is your age? ")) # Create an age variable. Wrap the input in int()
+# Same thing as age = int(age)
+
+if age < 3:
+    print(f"Your ticket is free.")
+elif age > 3 and age < 12:
+    print("Your ticket is $10.")
+else:
+    print("Your ticket is $15.")
