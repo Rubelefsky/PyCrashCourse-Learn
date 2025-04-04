e2bbcc60250bf2c27b2e186b610b90c0d1eb6b02
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-04 00:43:13 +0000

    add inifinity.py

---
+++ python_work/Chapter7 - Input/Chapter7Practice/inifity.py
@@ -0,0 +1,7 @@
+# 7.7 - infinity
+
+current_number = 1 # assign current_number to 1
+while current_number <= 5: # While current_number is <= 5
+    print(current_number) # print current_number (inside while loop)
+
+# Used the same infinity loop from page 117
--- python_work/Chapter7 - Input/Chapter7Practice/movie_tickets.py
+++ python_work/Chapter7 - Input/Chapter7Practice/movie_tickets.py
@@ -2,9 +2,9 @@
 age = int(input("What is your age? ")) # Create an age variable. Wrap the input in int()
 # Same thing as age = int(age)
 
-if age < 3:
-    print(f"Your ticket is free.")
-elif age > 3 and age < 12:
+if age < 3: # if age less than 3
+    print(f"Your ticket is free.") 
+elif age > 3 and age < 12: # If age more than 3 and less than 12
     print("Your ticket is $10.")
-else:
+else: 
     print("Your ticket is $15.")
