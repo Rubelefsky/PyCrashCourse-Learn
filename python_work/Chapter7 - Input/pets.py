6aa2e7e83f669c6597a456d719661458f9f1d01b
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-06 12:52:26 +0000

    pets.py

--- python_work/Chapter7 - Input/pets.py
+++ python_work/Chapter7 - Input/pets.py
@@ -1,7 +1,7 @@
-pets = ['dog', 'cat', 'goldfish', 'cat' , 'rabbit', 'cat']
+pets = ['dog', 'cat', 'goldfish', 'cat' , 'rabbit', 'cat'] # Create a list with multiple instances of cat
 print(pets)
 
-while 'cat' in pets:
-    pets.remove('cat')
+while 'cat' in pets: 
+    pets.remove('cat') # Loops through and removes each instance it finds and once it is not longer in the list prints
 
 print(pets)
