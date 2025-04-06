1d2c9207f17356f17e0c0b346642608e9d96a381
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-06 09:33:47 +0000

    adding files

---
+++ python_work/Chapter7 - Input/Chapter7Practice/three_exits.py
@@ -0,0 +1,8 @@
+# 7.6 
+
+age = int(input("What is your age? ")) # Create an age variable. Wrap the input in int()
+# Same thing as age = int(age)
+
+while age < 3:
+    print("You are not allowed to ride.")
+    
---
+++ python_work/Chapter7 - Input/confirmed_users.py
@@ -0,0 +1,3 @@
+# page 124
+
+# start with users that need to be verififed
