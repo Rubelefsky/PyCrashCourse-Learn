65ae37d64fff0c04db61cca4ecfd13f7b824e44e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-08 10:35:23 +0000

    deli.py added lists

--- python_work/Chapter7 - Input/Chapter7Practice/deli.py
+++ python_work/Chapter7 - Input/Chapter7Practice/deli.py
@@ -1,1 +1,9 @@
 # Page 127 7.8
+
+# Make a list called sandwich_orders / Fill with Various sandwiches
+sandwich_orders = ['BLT', 'Turkey Ranch', 'Roast Beef Hero', ' Chicken Parm']
+
+# Create an empty list called finished_sandwiches
+finished_sandwiches = []
+
+
