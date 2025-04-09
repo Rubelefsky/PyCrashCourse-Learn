488a655b81a70a692a814c5fc8ae202217625c4e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-09 12:02:14 +0000

    no_pastrami.py

---
+++ python_work/Chapter7 - Input/Chapter7Practice/no_pastrami.py
@@ -0,0 +1,5 @@
+# Page 126 7.9
+sandwich_orders = ['BLT', 'pastrami', 'Turkey Ranch', 'Roast Beef Hero', 'pastrami', 'Chicken Parm', 'pastrami'] # using list from 7.8, adding pastrami 3 times
+
+print("The deli has run out of pastrami.")
+
