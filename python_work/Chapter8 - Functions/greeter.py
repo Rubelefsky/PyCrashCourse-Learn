e82ff81fd8f0fadb2e594b05c2d115362eba5315
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-10 11:30:58 +0000

    add comments

--- python_work/Chapter8 - Functions/greeter.py
+++ python_work/Chapter8 - Functions/greeter.py
@@ -1,5 +1,5 @@
 def greet_user(): # def defines a function - name is greet_user
-    """Display a simple greeting."""
-    print("Hello!")
+    """Display a simple greeting.""" # docstring -decribes what the function does
+    print("Hello!") # Body of the function which is the actual code
 
-greet_user()
+greet_user() # Call the function
