8e011bfe4bcb7128496812ebe4236deab7e17033
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-10 16:27:53 +0000

    add title.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/title.py
@@ -0,0 +1,5 @@
+def favorite_book(book_title):
+    """Create a function that accepts one parameter, title."""
+    print(f"My favorite book is {book_title.title()}.")
+
+favorite_book('catcher in the rye')
