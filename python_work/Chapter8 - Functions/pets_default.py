a236bc6ff2090d8d8651bbf5f5d4f292cdfbfe36
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-15 00:14:32 +0000

    pets_default.py

--- python_work/Chapter8 - Functions/pets_default.py
+++ python_work/Chapter8 - Functions/pets_default.py
@@ -1,3 +1,10 @@
 # page 134
 
 # When writing a function, you can define a default value for each parameter
+def descibe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+descibe_pet(pet_name='willie')
+    
