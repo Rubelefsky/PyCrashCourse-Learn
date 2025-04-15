f7894a9a89f2a7198bdc0398ef93df2a68e3c2ab
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-15 00:49:30 +0000

    shirt.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/shirt.py
@@ -0,0 +1,7 @@
+# Page 136 8.3
+def make_shirt(size, message):
+    """Pick a size and create a message"""
+    print(f"\nYour shirt size is {size} and the message will say: {message}")
+
+make_shirt(size='large', message='Hello, this is my message!')
+make_shirt('medium', 'This is my medium shirt message!')
