0194e89c43bc4a8a855e00c125a7355de0a60e2e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-15 00:16:57 +0000

    adding comments

--- python_work/Chapter8 - Functions/pets_default.py
+++ python_work/Chapter8 - Functions/pets_default.py
@@ -7,4 +7,6 @@ def descibe_pet(pet_name, animal_type='dog'):
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 descibe_pet(pet_name='willie')
-    
+
+# When using default values, any parameter with a default value needs to be listed after all the parameters that don't have default values.
+# This allows python to continue interpreting positional arguments correctly.
