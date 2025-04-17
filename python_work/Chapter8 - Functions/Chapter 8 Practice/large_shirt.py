897445a1bb7dbeca8d90e8a589c24782e2b90a1a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-17 12:00:49 +0000

    made some changes and was looking at ematthes github for this section

--- python_work/Chapter8 - Functions/Chapter 8 Practice/large_shirt.py
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/large_shirt.py
@@ -1,9 +1,10 @@
 # page 136 8.4
 
-def make_shirt(size, message): # Create function
+def make_shirt(size='large', message='I love Python!'): # Create function
     """Create make shirt with a default value"""
     print(f"\nI am going to make a {size} t-shirt.")
     print(f"It will say, {message}")
 
-make_shirt('large', 'I love python!')
-make_shirt(message="Readability counts.", size='medium')
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Programmers are loopy.')
