9ce8c67fb6e60c55288aefbe20c182ad2b89a27e
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-18 19:48:04 +0000

    cities.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/cities.py
@@ -0,0 +1,9 @@
+# Page 137
+
+def describe_city(city, country='United States'):
+    """Describe a city and country"""
+    print(f"\n{city.title()} is located in {country.title()}")
+
+describe_city('New York')
+describe_city('Shanghai', 'China')
+describe_city('Naples', 'Italy')
