5a0a6175ff0821edb32da35d1d95501901082922
Brandon Rubell <brandonrubell@BRM4Pro.local>
2025-04-19 17:21:42 -0400

    person.py

---
+++ python_work/Chapter8 - Functions/person.py
@@ -0,0 +1,14 @@
+# page 138
+
+# Returning a dictionary
+def build_person(first_name, last_name, age=None):
+    """
+    Return a dictionary of information about a person.
+    """
+    person = {'first': first_name, 'last': last_name}# values are stored as a dictionary
+    if age:
+        person['age'] = age
+    return person # entire dictionary represeneting the person is returned
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
