c6e3875ca9e4d5675f59acd790b9353d746a3a76
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-20 20:26:10 +0000

    greeter_loop.py

--- python_work/Chapter8 - Functions/greeter_loop.py
+++ python_work/Chapter8 - Functions/greeter_loop.py
@@ -8,8 +8,15 @@ def get_formatted_name(first_name, last_name):
 # This is an infinite loop
 while True:
     print(f"\nPlease tell me your name.")
-    f_name = input("First name: ")
-    l_name = input("Last name: ")
+    print("(enter 'q' at any time to quit)")
 
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+# if q is entered in first or last name the program quits
     formatted_name = get_formatted_name(f_name, l_name)
     print(f"\nHello, {formatted_name}!")
