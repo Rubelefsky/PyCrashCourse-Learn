64fd9724fa1b236fd901272d27407fa33089476a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-21 22:23:18 +0000

    city, country.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/city_names.py
@@ -0,0 +1,10 @@
+# Page 141 
+# 8.6
+
+def city_country(city, country):
+    """Takes name of a city and country"""
+    print(f"{city.title()}, {country.title()}")
+
+city_country('new york', 'united states')
+city_country('milan', 'italy')
+city_country('paris', 'france')
