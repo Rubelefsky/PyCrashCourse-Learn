e39fa3f3f91eaf3e5ac320e81964e92d8d790a8c
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-23 18:44:55 +0000

    added changes

--- python_work/Chapter8 - Functions/Chapter 8 Practice/albums.py
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/albums.py
@@ -1,8 +1,15 @@
 # page 142 
 # 8.7
 
-def make_album(artist, album, number_of_songs=None):
+def make_album(artist_name, album_name, number_of_songs=None):
     """Function should take in an artist name and album title
     and return a dictionary containing these two pieces of info
     """
+    album_info = {'artist': artist_name, 'album': album_name}
+    if number_of_songs:
+        album_info['number_of_songs'] = number_of_songs
+    return album_info
+
+album1 = make_album('lloyd banks', 'cold corner 2', number_of_songs=15)
+print(album1)
 
