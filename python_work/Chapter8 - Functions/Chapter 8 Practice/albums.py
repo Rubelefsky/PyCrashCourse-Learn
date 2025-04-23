7cc776b92ba1f5e90ce6b2afd88386492c2defa0
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-23 17:55:42 +0000

    Started albums.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/albums.py
@@ -0,0 +1,8 @@
+# page 142 
+# 8.7
+
+def make_album(artist, album, number_of_songs=None):
+    """Function should take in an artist name and album title
+    and return a dictionary containing these two pieces of info
+    """
+
