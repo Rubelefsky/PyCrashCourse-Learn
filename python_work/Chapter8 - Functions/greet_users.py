426411149d63fdb3706661e0c9b8e8f1b94e4729
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-27 13:37:08 +0000

    greet_users.py

---
+++ python_work/Chapter8 - Functions/greet_users.py
@@ -0,0 +1,9 @@
+# Page 142 - Passing a list to a function
+def greet_users(names):
+    """Print a sample greeting to each user in the list."""
+    for name in names: # Cycle through the list
+        msg = f"Hello, {name.title()}!" # variable is set to say Hello and each name
+        print(msg) # print the variable
+
+usernames = ['hannah', 'ty', 'margot'] # create the list
+greet_users(usernames) # passes the list through the function
