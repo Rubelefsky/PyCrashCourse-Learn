ca9051b2f065b59e6efc81803ba865059916c0a2
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-04-27 13:05:11 +0000

    user_albums.py - need to go over functions again

--- python_work/Chapter8 - Functions/Chapter 8 Practice/user_albums.py
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/user_albums.py
@@ -9,4 +9,15 @@ def make_album(artist_name, album_name, number_of_songs=None):
         album_info['number_of_songs'] = number_of_songs
     return album_info
 
+while True:
+    print("\nWhat is your favorite Artist and Album?")
+    print("(enter 'q' at any time to quit)")
+
+    album_input = input("Album: ")
+    if album_input == 'q':
+        break
+
+    artist_input = input("Artist: ")
+    if artist_input == 'q':
+        break
     
