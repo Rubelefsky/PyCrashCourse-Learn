58ae225a0d04c12a7765451c465766288e81b4ef
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-05-06 12:01:01 +0000

    add messages.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/messages.py
@@ -0,0 +1,12 @@
+# page 146 8.9
+def show_messages(messages):
+    """
+    Make a list containing a series of short text messages. 
+    Pass the list to a function called show_messages(), which prints each text message
+    """
+    for message in messages:
+        print(message)
+    
+messages = ['Hello this is a message!']
+
+show_messages(messages)
