025be40ab5dd77da48a6b724fcd6fb37d644d28f
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-05-09 11:42:27 +0000

    sending_messages.py

--- python_work/Chapter8 - Functions/Chapter 8 Practice/sending_messages.py
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/sending_messages.py
@@ -1,9 +1,13 @@
 # Page 146
 # 8.10
 
-def send_messages(unsent_messages, sent_messages):
+def show_messages(messages):    
     """
    Start with a copy of your program from Exercise 8-9. 
    Write a function called send_messages() that prints each text message and moves each message to a new list called sent_messages as it’s printed. 
    After calling the function, print both of your lists to make sure the messages were moved correctly.
     """
+    for message in messages:
+        print(message)
+
+        
