4577fdbef940db20c8ba67b7c79e83ec55fc8ee4
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-05-12 11:14:52 +0000

    Starting sending_messages.py

--- python_work/Chapter8 - Functions/Chapter 8 Practice/sending_messages.py
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/sending_messages.py
@@ -1,13 +1,17 @@
 # Page 146
 # 8.10
 
-def show_messages(messages):    
+def show_messages(messages):    # Function called show_messages
     """
    Start with a copy of your program from Exercise 8-9. 
    Write a function called send_messages() that prints each text message and moves each message to a new list called sent_messages as it’s printed. 
    After calling the function, print both of your lists to make sure the messages were moved correctly.
     """
+    print("Showing all messages:")
     for message in messages:
         print(message)
 
-        
+
+messages = ["Hello, how are you?", "This is a python function"]
+
+show_messages(messages)
