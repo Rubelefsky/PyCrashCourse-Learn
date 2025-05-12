4d87d2934df196d47acb15c21bd80c620f108999
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-05-12 11:28:22 +0000

    completed sending_messages.py

--- python_work/Chapter8 - Functions/Chapter 8 Practice/sending_messages.py
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/sending_messages.py
@@ -11,7 +11,20 @@ def show_messages(messages):    # Function called show_messages
     for message in messages:
         print(message)
 
+def send_messages(messages, sent_messages):
+    """Print each message and move it to sent_messages"""
+    print("\nSending all messages:")
+    while messages:
+        current_message = messages.pop() 
+        print(current_message)
+        sent_messages.append(current_message)
 
 messages = ["Hello, how are you?", "This is a python function"]
-
 show_messages(messages)
+
+sent_messages = []
+send_messages(messages, sent_messages)
+
+print("\nFinal lists:")
+print(messages)
+print(sent_messages)
