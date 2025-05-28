41eae9d1673f28d887ae0ed2ba341afa4855949a
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-05-28 12:26:04 +0000

    cars.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/cars.py
@@ -0,0 +1,15 @@
+# Page 149
+def build_car(manufacturer, model, **kwargs): # kwargs = key word arguments
+    """
+    Write a function that stores information about a car in a diction- ary. 
+    The function should always receive a manufacturer and a model name. 
+    It should then accept an arbitrary number of keyword arguments. 
+    Call the func- tion with the required information and two other name-value pairs, such as a color or an optional feature. 
+    Your function should work for a call like this one:
+    """
+    kwargs['car_manufacturer'] = manufacturer
+    kwargs['car_model'] = model
+    return kwargs
+
+new_car = build_car('honda', 'accord', car_color='gray', seat_type='leather')
+print(new_car)
