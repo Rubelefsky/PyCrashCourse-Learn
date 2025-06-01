445245af6ae704b6705592fa226dcd8a7c08951f
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-06-01 21:35:56 +0000

    styling

---
+++ python_work/Chapter8 - Functions/styling_functions.py
@@ -0,0 +1,20 @@
+# Functions should have descriptive names
+# lower case and underscore
+# module names should use same conventions
+
+# Every function should have a comment to explain what it does
+# Comment should appear immediatley after the function definition and use the docstring format
+
+def function_name(parameter_0, parameter_1='default value'):
+    """
+    If you specify a default value for a parameter
+    No spaces should be used on either side of the equal sign
+    """
+
+function_name(value_0, parameter_1='value')
+# Same convention should be used for keyword arguments in function calls
+
+# Limit lines of code to 79 characters so every line is visible
+# https://peps.python.org/pep-0008/
+
+# See page 153 in Pycrashcourse to more styling
