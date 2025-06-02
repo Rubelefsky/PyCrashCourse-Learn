ff40f108938a3fa10967bd8ccddf7118e0c1b842
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-06-02 12:21:15 +0000

    imports.py

---
+++ python_work/Chapter8 - Functions/Chapter 8 Practice/imports.py
@@ -0,0 +1,23 @@
+# Page 154
+# 8.16
+# Using a program you wrote that has one function in it, store that function in a separate file. 
+# Import the function into your main program file, and call the function using each of these approaches:
+
+# import module_name 
+# from module_name import function_name 
+# from module_name import function_name as fn 
+# import module_name as mn 
+# from module_name import *
+
+import title # Import title.py
+
+from title import favorite_book # from module_name import function_name 
+from title import favorite_book as fb # from module_name import function_name as fn 
+import title as fb # import module_name as mn 
+from title import * # from module_name import *
+
+
+
+
+
+
