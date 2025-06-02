fc895e5cc4232568e74d77cb0405309d266fdf4f
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-06-02 12:36:44 +0000

    Chapter 9 Classes and dog.py

---
+++ python_work/Chapter9 - Classes/dog.py
@@ -0,0 +1,16 @@
+class Dog: # Define a class named dog. Capitalized names refer to classes in Python.
+    """A  simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting.")
+    
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
+        
