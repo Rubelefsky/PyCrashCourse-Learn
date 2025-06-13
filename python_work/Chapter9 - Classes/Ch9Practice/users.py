ad691e4acc751775d3dd2ad23716a1e48335f38c
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2025-06-13 18:26:45 +0000

    add users.py

--- python_work/Chapter9 - Classes/Ch9Practice/users.py
+++ python_work/Chapter9 - Classes/Ch9Practice/users.py
@@ -2,17 +2,27 @@
 class User:
     """Create 2 attributes"""
     
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, user_name):
         """First name and last name attributes"""
         self.first = first_name.title()
         self.last = last_name.title()
+        self.user = user_name
+
     
     def describe_user(self):
         """Print the description"""
-        user_info = f"First name is {self.first}"
+        user_info = f"First name is {self.first}. Last name is {self.last}. Username is {self.user}."
         print(f"\n{user_info}")
+    
+    def greet_user(self):
+        """Greet the user"""
+        full_name = f"{self.first} {self.last}"
+        print(f"Hello, {full_name}!")
 
-user_1 = User('brandon', 'rubell')
-user_1.describe_user()
+brandon = User('brandon', 'rubell', 'brubell')
+brandon.describe_user()
+brandon.greet_user()
+
+
 
 
