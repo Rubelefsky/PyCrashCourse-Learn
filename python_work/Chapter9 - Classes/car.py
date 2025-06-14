3e8a7374106f5b8cea5dc05e698ecc282ade3951
Brandon Rubell <brandonrubell@BRM4Pro.local>
2025-06-14 09:49:29 -0400

    Add odometer reading

--- python_work/Chapter9 - Classes/car.py
+++ python_work/Chapter9 - Classes/car.py
@@ -5,12 +5,17 @@ class Car: # Create car class
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0 # default value = 0
     
     def get_descriptive_name(self): # puts all info in 1 string
         """Return a neatly formatted descriptive name."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
     
+    def read_odemeter(self): # Print a statement showing the cars miles
+        """Odometer reading"""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
 my_new_car = Car('audi', 'r8', '2025') # my_new_car instance
 print(my_new_car.get_descriptive_name()) # call get_descriptive name with instance my_new_car
+my_new_car.read_odemeter()
